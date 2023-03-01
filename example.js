59b3a888dc0745893a55f1d528c5b9d9fd8e6bf9
Thorben Ziemek <thorben.ziemek@brokerize.com>
2023-03-01 16:03:01 +0100

    chore: configure example to use latest elements version, add more view examples

--- example.js
+++ example.js
@@ -4,6 +4,7 @@
  * The following typedef imports enable code completion in VS Code:
  */
 /** @typedef {import("@brokerize/elements").Client} BrokerizeClient} */
+/** @typedef {import("@brokerize/elements").Client} BrokerizeClient} */
 /** @typedef {import("@brokerize/elements").Elements} BrokerizeElements} */
 /** @typedef {import("@brokerize/elements").BrokerizeElement} BrokerizeElement} */
 /** @typedef {{ Client: BrokerizeClient; Elements:BrokerizeElements }} BrokerizeBundle} */
@@ -12,30 +13,40 @@ const Brokerize = /** @type {BrokerizeBundle} */ (window.Brokerize);
 const config = window.BROKERIZE_CONFIG;
 
 if (!config || !config.CLIENT_ID) {
-	alert('no config could be found. Please include config.js in your HTML file.');
-	throw new Error("No config provided");
+    alert(
+        "no config could be found. Please include config.js in your HTML file."
+    );
+    throw new Error("No config provided");
 }
 
 const client = new Brokerize.Client.Brokerize({
-	// API configuration
-	basePath: config.API_URL,
-	clientId: config.CLIENT_ID,
-	cognito: {
-		UserPoolId: 'eu-central-1_jRMDxLPQW',
-		ClientId: config.COGNITO_CLIENT_ID,
-		Endpoint: null
-	},
-	// provide global dependencies
-	fetch: window.fetch.bind(window),
-	createAbortController: () => new AbortController(),
-	createWebSocket: (url, protocol) => new WebSocket(url, protocol)
+    // API configuration
+    basePath: config.API_URL,
+    clientId: config.CLIENT_ID,
+    cognito: {
+        UserPoolId: "eu-central-1_jRMDxLPQW",
+        ClientId: config.COGNITO_CLIENT_ID,
+        Endpoint: null,
+    },
+    // provide global dependencies
+    fetch: window.fetch.bind(window),
+    createAbortController: () => new AbortController(),
+    createWebSocket: (url, protocol) => new WebSocket(url, protocol),
 });
 
 /* this changes when the user logs in/out of brokerize or starts/ends guest sessions */
 let globalApiCtx = null;
 
 /* let's render everything in the #content element */
-const $el = document.getElementById('content');
+const $el = document.getElementById("content");
+
+function setLastUsedPortfolio(id) {
+    window.localStorage.setItem("lastportfolio", id);
+}
+
+function getLastUsedPortfolio() {
+    return window.localStorage.getItem("lastportfolio")||null;
+}
 
 /**
  * @type {BrokerizeElement}
@@ -44,258 +55,344 @@ let currentElement;
 
 let currentPortfolioId = null;
 function setCurrentPortfolioId(id) {
-	currentPortfolioId = id;
-	document.getElementById('big-buy-button').style.display = id
-		? 'inline'
-		: 'none';
+    currentPortfolioId = id;
+    document.getElementById("big-buy-button").style.display = id
+        ? "inline"
+        : "none";
 }
 
 function resetRenderTo() {
-	currentElement && currentElement.destroy();
-	setCurrentPortfolioId(null);
-	$el.innerHTML = '';
-	return $el;
+    currentElement && currentElement.destroy();
+    setCurrentPortfolioId(null);
+    $el.innerHTML = "";
+    return $el;
 }
 
 function showError(err) {
-	alert('ERROR: ' + JSON.stringify(err));
+    alert("ERROR: " + JSON.stringify(err));
 }
 
 function setLogin(authCtxCfg) {
-	sessionStorage.setItem('brokerize', JSON.stringify(authCtxCfg));
-	initSessionIf();
+    sessionStorage.setItem("brokerize", JSON.stringify(authCtxCfg));
+    initSessionIf();
 }
 
 function cleanUpUrl() {
-	const url = new URL(window.location.href);
-	for (const key of ['verifysession', 'code', 'ticketId']) {
-		url.searchParams.delete(key);
-	}
-	window.location.replace(url);
+    const url = new URL(window.location.href);
+    for (const key of ["verifysession", "code", "ticketId"]) {
+        url.searchParams.delete(key);
+    }
+    window.location.replace(url);
 }
 
 /* restore session from sessionStorage, if possible */
 function initSessionIf() {
-	const scfg = sessionStorage.getItem('brokerize');
-	const cfg = scfg ? JSON.parse(scfg) : null;
+    const scfg = sessionStorage.getItem("brokerize");
+    const cfg = scfg ? JSON.parse(scfg) : null;
 
-	if (!cfg) {
-		showLogin();
-	} else {
-		globalApiCtx = client.createAuthorizedContext(cfg);
-		globalApiCtx.subscribeLogout((err)=>{
-			console.log(err, 'guest user has been logged out from brokerize API.');
-			showLogin();
-		});
+    if (!cfg) {
+        showLogin();
+    } else {
+        globalApiCtx = client.createAuthorizedContext(cfg);
+        globalApiCtx.subscribeLogout((err) => {
+            console.log(
+                err,
+                "guest user has been logged out from brokerize API."
+            );
+            showLogin();
+        });
 
-		const urlParams = new URLSearchParams(window.location.search);
-		if (urlParams.get('verifysession')) {
-			const code = urlParams.get('code');
-			const ticketId = urlParams.get('ticketId');
-			globalApiCtx.confirmOAuth({ code, ticketId }).then(
-				() => {
-					cleanUpUrl();
-					showSessionsTable();
-				},
-				(err) => {
-					showError(
-						'An error occured when trying to confirm the OAuth-based broker login: ' +
-							JSON.stringify(err)
-					);
-					cleanUpUrl();
-				}
-			);
-		} else {
-			showBrokerList();
-		}
-	}
+        const urlParams = new URLSearchParams(window.location.search);
+        if (urlParams.get("verifysession")) {
+            const code = urlParams.get("code");
+            const ticketId = urlParams.get("ticketId");
+            globalApiCtx.confirmOAuth({ code, ticketId }).then(
+                () => {
+                    cleanUpUrl();
+                    showSessionsTable();
+                },
+                (err) => {
+                    showError(
+                        "An error occured when trying to confirm the OAuth-based broker login: " +
+                            JSON.stringify(err)
+                    );
+                    cleanUpUrl();
+                }
+            );
+        } else {
+            showBrokerList();
+        }
+    }
 }
 
 /* theme configuration. example themes are available under https://app.brokerize.com/theming/ */
 const renderConfig = {
-	theme: {
-		name: 'Default',
-		icon: 'circlehollow',
-		id: 'default',
-		layout: 'columns',
-		logoStyle: 'light',
-		tokens: {
-			'zl-border-radius': '.3rem',
-			'zl-notification-bg-color': 'var(--zl-colors-dark1)'
-			/* ...many more tokens are available (see theming tool) */
-		}
-	},
-	/* brokerize elements may load required assets like stylesheets. this should be the public path
+    theme: {
+        name: "Default",
+        icon: "circlehollow",
+        id: "default",
+        layout: "columns",
+        logoStyle: "light",
+        tokens: {
+            "zl-border-radius": ".3rem",
+            "zl-notification-bg-color": "var(--zl-colors-dark1)",
+            /* ...many more tokens are available (see theming tool) */
+        },
+    },
+    /* brokerize elements may load required assets like stylesheets. this should be the public path
 	   to the brokerize elements "dist" directory */
-	assetsPath: "/node_modules/@brokerize/elements/dist"
-}
-
+    assetsPath: "/node_modules/@brokerize/elements/dist",
+};
 
 function showBrokerLogin(brokerName) {
-	Brokerize.Elements.createBrokerLoginForm({
-		renderTo: resetRenderTo(),
-		renderConfig,
-		brokerName,
-		authorizedApiContext: globalApiCtx,
-		onExit({ loggedIn }) {
-			alert('Login erfolgreich?' + loggedIn);
-			resetRenderTo();
-		}
-	});
+    Brokerize.Elements.createBrokerLoginForm({
+        renderTo: resetRenderTo(),
+        renderConfig,
+        brokerName,
+        authorizedApiContext: globalApiCtx,
+        onExit({ loggedIn }) {
+            showPortfolioTable();
+        },
+    });
 }
 
 function showBrokerList() {
-	if (!globalApiCtx) {
-		return alert('you must authorize first.');
-	}
+    if (!globalApiCtx) {
+        return alert("you must authorize first.");
+    }
 
-	Brokerize.Elements.createBrokerList({
-		renderConfig,
-		renderTo: resetRenderTo(),
-		authorizedApiContext: globalApiCtx,
-		onLogin({ brokerName }) {
-			showBrokerLogin(brokerName);
-		}
-	});
+    Brokerize.Elements.createBrokerList({
+        renderConfig,
+        renderTo: resetRenderTo(),
+        authorizedApiContext: globalApiCtx,
+        onLogin({ brokerName }) {
+            showBrokerLogin(brokerName);
+        },
+    });
 }
 
 function buy() {
-	const isin = prompt(
-		'enter ISIN that you want to trade in the current portfolio please',
-		'US0378331005'
-	);
-	if (isin) {
-		showOrderForm(currentPortfolioId, isin);
-	}
+    const isin = prompt(
+        "enter ISIN that you want to trade in the current portfolio please",
+        "US0378331005"
+    );
+    if (isin) {
+        showOrderForm(currentPortfolioId, isin, {
+            direction: "buy",
+        });
+    }
 }
 
 function showPortfolioTable() {
-	if (!globalApiCtx) {
-		return alert('you must authorize first.');
-	}
+    if (!globalApiCtx) {
+        return alert("you must authorize first.");
+    }
 
-	Brokerize.Elements.createPortfolioTable({
-		renderConfig,
-		renderTo: resetRenderTo(),
-		authorizedApiContext: globalApiCtx,
-		onNavigate(portfolio) {
-			showPortfolioView(portfolio.id);
-		}
-	});
+    Brokerize.Elements.createPortfolioTable({
+        renderConfig,
+        renderTo: resetRenderTo(),
+        authorizedApiContext: globalApiCtx,
+        onNavigate(portfolio) {
+            showPortfolioView(portfolio.id);
+        },
+    });
 }
 
 function showPortfolioView(portfolioId) {
-	if (!globalApiCtx) {
-		return alert('you must authorize first.');
-	}
+    if (!globalApiCtx) {
+        return alert("you must authorize first.");
+    }
 
-	Brokerize.Elements.createPortfolioView({
-		renderConfig,
-		renderTo: resetRenderTo(),
-		authorizedApiContext: globalApiCtx,
-		portfolioId,
-		onBuy(opts) {
-			alert('TODO: show buy form for ' + opts.isin);
-		},
-		onSell(opts) {
-			alert(
-				'TODO: show sell form for ' +
-					opts.isin +
-					' (current availableSize: ' +
-					opts.availableSize +
-					')'
-			);
-		},
-		onCancelOrder(opts) {
-			alert('TODO: show cancel order process for ' + opts.orderId);
-		},
-		onChangeOrder(opts) {
-			alert('TODO: show change order process for ' + opts.orderId);
-		},
-		onShowReceipt(opts) {
-			alert('TODO: show receipt for ' + opts.orderId);
-		}
-	});
-	setCurrentPortfolioId(portfolioId);
+    setLastUsedPortfolio(portfolioId);
+
+    Brokerize.Elements.createPortfolioView({
+        renderConfig,
+        renderTo: resetRenderTo(),
+        authorizedApiContext: globalApiCtx,
+        portfolioId,
+        onBuy(opts) {
+            showOrderForm(portfolioId, opts.isin, { direction: "buy" });
+        },
+        onSell(opts) {
+            showOrderForm(portfolioId, opts.isin, {
+                direction: "sell",
+                size: opts.availableSize,
+            });
+        },
+        onCancelOrder(opts) {
+            showCancelOrderForm(portfolioId, opts.orderId);
+        },
+        onChangeOrder(opts) {
+            showChangeOrderForm(opts.orderId);
+        },
+        onShowReceipt(opts) {
+            showReceipt(opts.orderId);
+        },
+    });
+    setCurrentPortfolioId(portfolioId);
+}
+
+function showReceipt(orderId) {
+    Brokerize.Elements.createOrderReceipt({
+        renderConfig,
+        renderTo: resetRenderTo(),
+        authorizedApiContext: globalApiCtx,
+
+        orderId,
+        onNavigate: (linkTarget) => {
+            debugger
+            showPortfolioView(linkTarget.portfolioId);
+        },
+    });
+}
+
+function showSessionTanForm(sessionId) {
+    Brokerize.Elements.createSessionTanForm({
+        sessionId,
+        renderConfig,
+        renderTo: resetRenderTo(),
+        authorizedApiContext: globalApiCtx,
+    });
 }
 
 function showSessionsTable() {
-	if (!globalApiCtx) {
-		return alert('you must authorize first.');
-	}
+    if (!globalApiCtx) {
+        return alert("you must authorize first.");
+    }
 
-	Brokerize.Elements.createSessionsTable({
-		renderConfig,
-		renderTo: resetRenderTo(),
-		authorizedApiContext: globalApiCtx,
-		onEnableSessionTan({ sessionId }) {
-			alert('TODO: show a modal with createSessionTanForm.');
-		}
-	});
+    Brokerize.Elements.createSessionsTable({
+        renderConfig,
+        renderTo: resetRenderTo(),
+        authorizedApiContext: globalApiCtx,
+        onEnableSessionTan({ sessionId }) {
+            showSessionTanForm(sessionId);
+        },
+    });
 }
 
-function showOrderForm(portfolioId, isin) {
-	if (!globalApiCtx) {
-		return alert('you must authorize first.');
-	}
+function showCancelOrderForm(portfolioId, orderId) {
+    Brokerize.Elements.createCancelOrderForm({
+        renderConfig,
+        renderTo: resetRenderTo(),
+        authorizedApiContext: globalApiCtx,
 
-	Brokerize.Elements.createOrderForm({
-		renderConfig,
-		renderTo: resetRenderTo(),
-		authorizedApiContext: globalApiCtx,
+        orderId,
+        portfolioId,
+        onExit: () => resetRenderTo(),
+        onNavigate: (linkTarget) => {
+            debugger;
+            showPortfolioView(linkTarget.portfolioId);
+        },
+    });
+}
 
-		portfolioId,
-		isin,
+function showChangeOrderForm(orderId) {
+    Brokerize.Elements.createChangeOrderForm({
+        renderConfig,
+        renderTo: resetRenderTo(),
+        authorizedApiContext: globalApiCtx,
 
-		// preferredExchangeId: 4, // a preferred exchange to pre-select in the OrderFor (if allowed) (XETRA: 4, Nasdaq: 45, NYSE: 21, ...)
-		// initialOrder: {
-		// 	/* may set default values for direction, orderModel, limit, stop, stopLimit, stopLoss, validity */
-		// 	direction: 'sell',
-		// 	orderModel: 'limit',
-		// 	limit: 25,
-		// 	validity: {
-		// 		type: 'GFD'
-		// 	}
-		// },
+        orderId,
+        onExit: () => resetRenderTo(),
+        onNavigate: (linkTarget) => {
+            debugger;
+            showPortfolioView(linkTarget.portfolioId);
+        },
+    });
+}
 
-		onOrderCreated(createdTrade) {
-			alert('order created: ' + JSON.stringify(createdTrade));
-			resetRenderTo();
-		}
-	});
+function showOrderForm(portfolioId, isin, initialOrder) {
+    if (!globalApiCtx) {
+        return alert("you must authorize first.");
+    }
+
+    Brokerize.Elements.createOrderForm({
+        renderConfig,
+        renderTo: resetRenderTo(),
+        authorizedApiContext: globalApiCtx,
+
+        portfolioId,
+        isin,
+
+        initialOrder,
+
+        // preferredExchangeId: 4, // a preferred exchange to pre-select in the OrderFor (if allowed) (XETRA: 4, Nasdaq: 45, NYSE: 21, ...)
+        // initialOrder: {
+        // 	/* may set default values for direction, orderModel, limit, stop, stopLimit, stopLoss, validity */
+        // 	direction: 'sell',
+        // 	orderModel: 'limit',
+        // 	limit: 25,
+        // 	validity: {
+        // 		type: 'GFD'
+        // 	}
+        // },
+
+        onOrderCreated(createdTrade) {
+            showReceipt(createdTrade.orderId);
+        },
+    });
 }
 
 function logInAsGuest() {
-	console.log('logging in as guest');
-	client.createGuestUser().then(
-		(authCtxCfg) => {
-			setLogin(authCtxCfg);
-		},
-		(err) => showError(err)
-	);
+    console.log("logging in as guest");
+    client.createGuestUser().then(
+        (authCtxCfg) => {
+            setLogin(authCtxCfg);
+        },
+        (err) => showError(err)
+    );
 }
 
 function showLogin() {
-	if (config.COGNITO_CLIENT_ID) {
-		/* the client supports brokerize user logins. */
-		Brokerize.Elements.createLoginForm({
-			renderTo: resetRenderTo(),
-			renderConfig,
-			client,
-			onGuestLogin() {
-				logInAsGuest();
-			},
-			onLogin(authCtxCfg) {
-				setLogin(authCtxCfg);
-			}
-		});
-	} else {
-		/* the client only supports guest users */
-		logInAsGuest();
-	}
-	
+    if (config.COGNITO_CLIENT_ID) {
+        /* the client supports brokerize user logins. */
+        Brokerize.Elements.createLoginForm({
+            renderTo: resetRenderTo(),
+            renderConfig,
+            client,
+            onGuestLogin() {
+                logInAsGuest();
+            },
+            onLogin(authCtxCfg) {
+                setLogin(authCtxCfg);
+            },
+        });
+    } else {
+        /* the client only supports guest users */
+        logInAsGuest();
+    }
 }
 
-/* ... more calls to Brokerize.Elements.createXXXXXX to be added here ... */
+/**
+ * Helper function for selecting the last used portfolio of the user, if the user is logged in to
+ * to that portfolio.
+ *
+ * If that is not the case, select the first portfolio that can be used for trading.,
+ *
+ * Otherwise, returns null.
+ */
+async function selectPortfolio(lastUsedId) {
+    const { portfolios } = await globalApiCtx.getPortfolios();
+    const onlinePortfolioIds = portfolios
+        .filter((p) => p.sessionIds.length > 0)
+        .map((p) => p.id);
+    if (lastUsedId && onlinePortfolioIds.includes(lastUsedId)) {
+        return lastUsedId;
+    }
+    return onlinePortfolioIds.length ? onlinePortfolioIds[0] : null;
+}
+
+async function startExampleOrderFlow() {
+    const portfolioId = await selectPortfolio(getLastUsedPortfolio());
+    if (!portfolioId) {
+        showBrokerList();
+    } else {
+        showOrderForm(portfolioId, 'US0378331005', {
+            direction: "buy",
+            orderModel: "limit",
+            limit: 15,
+        });
+    }
+}
 
 initSessionIf();
